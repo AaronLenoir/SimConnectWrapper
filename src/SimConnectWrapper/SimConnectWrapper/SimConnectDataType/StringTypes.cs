f818a45979e02ecf542c39492b899278530d59e2
AaronLenoir <lenoir.aaron@gmail.com>
2021-02-10 23:53:39 +0100

    Add object model and generic interface

---
+++ src/SimConnectWrapper/SimConnectWrapper/SimConnectDataType/StringTypes.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SimConnectWrapper.SimConnectDataType
+{
+    // SimConnect exposes various fixed sized strings which can be
+    // marshalled to a .NET String
+
+    public struct String8
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public string Value;
+    }
+    public struct String32
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string Value;
+    }
+
+    public struct String64
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public string Value;
+    }
+
+    public struct String128
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string Value;
+    }
+
+    public struct String256
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string Value;
+    }
+
+    public struct String260
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string Value;
+    }
+}
---
+++ src/SimConnectWrapper/SimConnectWrapper/SimConnectPropertyKey.cs
@@ -0,0 +1,14 @@
+﻿namespace SimConnectWrapper
+{
+    public enum SimConnectPropertyKey
+    {
+        Unknown = 0,
+        PlaneLongitude = 1,
+        PlaneLatitude = 2,
+        PlaneHeadingDegreesTrue = 3,
+        PlaneAltitude = 4,
+        GpsGroundSpeed = 5,
+        AtcId = 6,
+        AtcType = 7
+    }
+}
